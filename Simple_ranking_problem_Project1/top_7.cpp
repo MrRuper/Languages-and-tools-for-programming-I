7779d7b7443a9820e849e0ecab15e175d8f90e2f
Bogdan Petraszczuk <bp372955@students.mimuw.edu.pl>
2023-09-26 00:42:24 +0200

    Added fixed solution and for now 1 test

--- Simple_ranking_problem_Project1/top_7.cpp
+++ Simple_ranking_problem_Project1/top_7.cpp
@@ -28,13 +28,27 @@ std::vector<int> remember_last_NEW_MAX;
 // This will hold the previous TOP result.
 std::vector<int> remember_last_TOP;
 
+// Global constant for keeping 
+// range of NEW command.
 long long int MAX = -1;
 
-bool sort_by_sec_desc(const std::pair<int,int> &a, const std::pair<int,int> &b) {
-        return a.second > b.second;
+// Global constant describing 
+// maximum number of songs in ranked.
+const uint64_t MAX_SONGS = 7;
+
+// This function compairs two pairs {x,y}, {c,d}:
+// {x,y} < {c,d} iff y < d or  (y == d) and x < c.
+bool sort_by_sec_desc(const std::pair<int, int>& a, const std::pair<int, int>& b) {
+    if (a.second != b.second) {
+  	// Sort by total_score in descending order
+        return a.second > b.second; 
+    }
+    
+    // If total_score is the same, sort by song number in ascending order
+    return a.first < b.first; 
 }
 
-// Fix: the higher position takes song with less number.
+// Process NEW_MAX command.
 void NEW_MAX_command(const int NEW_MAX) {
     std::vector<std::pair<int,int>> song_score(current_ranking.size());
 
@@ -45,30 +59,31 @@ void NEW_MAX_command(const int NEW_MAX) {
         index++;
     }
 
-    // Firstly sort with respect to the song_number,
-    // secondly sort with respect to the number_of_points.
-    // This will ensure that if two songs x, y have the same number of
-    // points, min{x,y} will be before max{x,y}.
-    std::sort(song_score.begin(), song_score.end());
+    // Sort to get 7 (or less) best songs.
     std::sort(song_score.begin(), song_score.end(), sort_by_sec_desc);
-
-    std::vector<int> seven_best_songs(std::min((uint64_t) 7, song_score.size()));
-
-    for (int i = 0; i < seven_best_songs.size(); i++) {
+	
+	// Create vector for keeping current best songs.    
+    std::vector<int> seven_best_songs(std::min(MAX_SONGS, song_score.size()));
+	
+    for (uint64_t i = 0; i < seven_best_songs.size(); i++) {
         seven_best_songs[i] = song_score[i].first;
     }
 
-    for (int i = 0; i < seven_best_songs.size(); i++) {
+	// Print out the ranking result with checking for each song
+	// it's old position in previous ranking.
+    for (uint64_t i = 0; i < seven_best_songs.size(); i++) {
         int current_song_number = song_score[i].first;
 
         bool song_found_in_last_ranked = false;
-
-        for (int j = 0; j < remember_last_NEW_MAX.size(); j++) {
+		
+		// Find this song in previous ranking.
+        for (uint64_t j = 0; j < remember_last_NEW_MAX.size(); j++) {
             if (remember_last_NEW_MAX[j] == current_song_number) {
                 song_found_in_last_ranked = true;
-
+				long long int difference = j - i;
+		
                 // Change of position = i - j.
-                std::cout << current_song_number << " " << j - i << std::endl;
+                std::cout << current_song_number << " " << difference << std::endl;
             }
         }
 
@@ -77,15 +92,15 @@ void NEW_MAX_command(const int NEW_MAX) {
         }
 
         // Update TOP result.
-        history_ranking[current_song_number] += 7 - i;
+        history_ranking[current_song_number] += MAX_SONGS - i;
     }
 
     // Update removed_song.
-    for (int i = 0; i < remember_last_NEW_MAX.size(); i++) {
+    for (uint64_t i = 0; i < remember_last_NEW_MAX.size(); i++) {
         int song_number = remember_last_NEW_MAX[i];
         bool passed_to_next_ranking = false;
 
-        for (int j = 0; j < seven_best_songs.size(); j++) {
+        for (uint64_t j = 0; j < seven_best_songs.size(); j++) {
             if (song_number == seven_best_songs[j]) {
                 passed_to_next_ranking = true;
             }
@@ -115,13 +130,16 @@ void TOP_command() {
         song_general_score[counter] = i;
         counter++;
     }
-
-    std::sort(song_general_score.begin(), song_general_score.end());
+	
+	// Sort to get seven (or less) best songs in total.
     std::sort(song_general_score.begin(), song_general_score.end(), sort_by_sec_desc);
+	
+	// Create vector to remember current best TOP.
+    std::vector<int> seven_best_for_TOP(std::min(MAX_SONGS, song_general_score.size()));
 
-    std::vector<int> seven_best_for_TOP(std::min((uint64_t) 7, song_general_score.size()));
-
-    for (int i = 0; i < seven_best_for_TOP.size(); i++) {
+	// Print out result with checking for each song in a TOP its
+	// old position in previous TOP.
+    for (uint64_t i = 0; i < seven_best_for_TOP.size(); i++) {
         int current_song_number = song_general_score[i].first;
         bool appeared_previously = false;
 
@@ -131,7 +149,9 @@ void TOP_command() {
         for (auto j : remember_last_TOP) {
             if (j == current_song_number) {
                 appeared_previously = true;
-                std::cout << current_song_number << " " << counter - i << std::endl;
+                long long difference = counter - i;
+                
+                std::cout << current_song_number << " " << difference << std::endl;
             }
 
             counter++;
@@ -146,13 +166,16 @@ void TOP_command() {
     song_general_score.clear();
     remember_last_TOP.clear();
 
+	// Update last TOP result.
     remember_last_TOP = seven_best_for_TOP;
 }
 
+// Print out the error line.
 void print_error(std::string& invalid_string, uint64_t line_number) {
     std::cerr << "Error in line " << line_number << ": " << invalid_string << std::endl;
 }
 
+// Checks wheter song number is valid.
 bool invalid_song(long long int song_number) {
     if (removed_songs.find(song_number) != removed_songs.end() ||
         song_number > 99999999 ||
@@ -163,20 +186,34 @@ bool invalid_song(long long int song_number) {
     return false;
 }
 
+// This method takes line from a standard input and 
+// cheks if this is a good vote and if it is so - updates
+// all needed stuff for NEW_MAX or TOP command. If the line
+// is bad then is clearing all additional memory, prints out 
+// an error line and returns.
 void process_vote(std::string& line, uint64_t line_number) {
+	// This pattern matches everything except white spaces.
     std::regex pattern("[^\\s]+");
-    std::set<int> check_different;
+    
+    // This set is needed in processing the vote vector.
+    std::set<long long int> check_different;
+    
+    // Vector for holding the vote.
     std::vector<int> vote;
     long long int song;
     int NEW_MAX;
 
+	// iterator will go through every subword of the given word.
+	// Subwords are splitted by white space/s.
     auto words_begin = std::sregex_iterator(line.begin(),
                                             line.end(),
                                             pattern);
     auto words_end = std::sregex_iterator();
 
+	// Number describing how much subwords we have got.
     long long int length_of_word = std::distance(words_begin, words_end);
 
+	// Start working with input.
     if (length_of_word == 1) {
         if (words_begin->str() == "TOP") {
             TOP_command();
@@ -266,8 +303,8 @@ void process_vote(std::string& line, uint64_t line_number) {
                 print_error(line, line_number);
             }
 
-            if (invalid_song(song) ||
-                check_different.find(song) != check_different.end()) {
+            if (invalid_song(song) || 
+            	(check_different.find(song) != check_different.end())) {
                 print_error(line, line_number);
                 check_different.clear();
                 vote.clear();
@@ -280,10 +317,13 @@ void process_vote(std::string& line, uint64_t line_number) {
         }
     }
 
+	// Update current_ranking by adding +1 to 
+	// each number appeared in vote.
     for (auto z : vote) {
         current_ranking[z]++;
     }
 
+	// Clear all additional memory.
     vote.clear();
     check_different.clear();
 }
